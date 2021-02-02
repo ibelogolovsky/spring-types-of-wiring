10a34601d64943ba946c421dc24c36dbd7e26c2e
Igor Belogolovsky <igor@belogolovsky.net>
2021-02-02 09:27:32 +0300

    Task solution

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    //Связывание через сеттер
+    private Death8 death;
+
+    @Autowired
+    public void setDeath(Death8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
